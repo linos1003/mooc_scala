dd95332908d88af7713dea8ca508b0a6927bfa7a
linos <smida.bassem@gmail.com>
2017-04-20 17:23:11 +0200

    pascal method

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -2,24 +2,31 @@ package recfun
 
 object Main {
   def main(args: Array[String]) {
-    println("Pascal's Triangle")
-    for (row <- 0 to 10) {
-      for (col <- 0 to row)
-        print(pascal(col, row) + " ")
-      println()
-    }
+//    println("Pascal's Triangle")
+//    for (row <- 0 to 10) {
+//      for (col <- 0 to row)
+//        print(pascal(col, row) + " ")
+//      println()
+//    }
+    
   }
 
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+
+    def pascal(c: Int, r: Int): Int = {
+      if(c==0 || r==c) 1
+      else pascal(c,r-1)+pascal(c-1,r-1)
+    }
+
+
   
   /**
    * Exercise 2
    */
     def balance(chars: List[Char]): Boolean = ???
-  
+
   /**
    * Exercise 3
    */
